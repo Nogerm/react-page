86c5ef529124cc27b6aec9a92119d607c64215ef
Andy CW Wu <andy_cw_wu@wistron.com>
2018-10-18 10:40:52 +0800

    Updates

---
+++ precache-manifest.59fd5f0f761cc4670c57f8ce6bc19b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43eb6a92e2f3418f7a49",
+    "url": "/react-page/static/js/runtime~main.43eb6a92.js"
+  },
+  {
+    "revision": "88c16e05481e13044dc5",
+    "url": "/react-page/static/js/main.88c16e05.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-page/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "88c16e05481e13044dc5",
+    "url": "/react-page/static/css/main.eab36ab3.chunk.css"
+  },
+  {
+    "revision": "24694908199a942c94cda596306b7573",
+    "url": "/react-page/index.html"
+  }
+];
