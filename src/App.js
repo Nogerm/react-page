1beebb42fb3ef1215d0c751b12d4896bf384352e
Andy CW Wu <andy_cw_wu@wistron.com>
2018-12-10 13:04:34 +0800

    update html page

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Query Mongodb Data</p>
           <a
             className="App-link"
             href="https://reactjs.org"
