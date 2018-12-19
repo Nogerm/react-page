84c9ee53add32c29dbb9f38fd3f3ffd08be711e2
Andy_CW_Wu <andy_cw_wu@wistron.com>
2018-12-19 10:48:18 +0800

    Add menu bar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Container, LeftColumn, RightColumn, LogoImage, ContentText } from './style';
+import { LogoImage } from './style';
+import { Grid, Menu, Header } from 'semantic-ui-react'
 import Birthday from './../Birthday';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
 
+  state = { 
+    activeItem: 'birthday' 
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <Container>
-        <LeftColumn>
+      <Grid>
+        <Grid.Column width={3} style={{ 'background': '#4682B4', 'padding-right': 0 }}>
           <LogoImage src={logo}/>
-            <ContentText>Edit <code>src/App.js</code> and save to reload.</ContentText>
-            <ContentText>Query Mongodb Data</ContentText>
-        </LeftColumn>
-        <RightColumn>
+          <Header as='h2' style={{ 'margin': '10px', 'color': 'white' }}>Control Center</Header>
+          <Header as='h4' style={{ 'margin': '10px', 'color': 'white' }}>The Backend of Linebot.</Header>
+          <Menu fluid vertical tabular style={{ 'margin-top': '50px' }}>
+            <Menu.Item 
+              name='birthday' 
+              active={activeItem === 'birthday'} 
+              onClick={this.handleItemClick} 
+            />
+            <Menu.Item 
+              name='routine' 
+              active={activeItem === 'routine'} 
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='bless'
+              active={activeItem === 'bless'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+        </Grid.Column>
+
+        <Grid.Column stretched width={12}>
           <Birthday/>
-        </RightColumn>
-      </Container>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
--- src/Components/App/style.js
+++ src/Components/App/style.js
@@ -10,33 +10,6 @@ const LogoMove = keyframes`
   }
 `;
 
-export const Container = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  justify-content: center;
-  display: flex;
-  flex-direction: row;
-`;
-
-export const LeftColumn = styled.div`
-	flex: 2;
-	justify-content: center;
-	background: #282c34;
-	display: flex;
-  flex-direction: column;
-`;
-
-export const RightColumn = styled.div`
-	flex: 5;
-	justify-content: center;
-	display: flex;
-  flex-direction: row;
-`;
-
-export const ContentText = styled.p`
-	color: white;
-`;
-
 export const LogoImage = styled.img`
 	height: 75px;
 	width: 75px;
