7db29cdb818bb7e37c8c07a4e06dcd5bc30b14ea
Andy CW Wu <andy_cw_wu@wistron.com>
2018-12-25 18:19:54 +0800

    Updates

--- precache-manifest.7a6631107b818cfca4b3372c86279c56.js
+++ precache-manifest.7a6631107b818cfca4b3372c86279c56.js
@@ -1,55 +1,63 @@
 self.__precacheManifest = [
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/react-page/static/media/icons.b87b9ba5.ttf"
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-page/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "e76e4f58ef82c6731d07",
-    "url": "/react-page/static/css/main.e7147810.chunk.css"
+    "revision": "99b26d68f712917e7c8c",
+    "url": "/react-page/static/css/main.e1002214.chunk.css"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
     "url": "/react-page/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "e5ace7fcc45b60c66e6e",
-    "url": "/react-page/static/js/1.e5ace7fc.chunk.js"
+    "revision": "f6a924b1c43665667956",
+    "url": "/react-page/static/js/1.f6a924b1.chunk.js"
   },
   {
     "revision": "43eb6a92e2f3418f7a49",
     "url": "/react-page/static/js/runtime~main.43eb6a92.js"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/react-page/static/media/icons.faff9214.woff"
-  },
-  {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/react-page/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-page/static/media/icons.faff9214.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/react-page/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/react-page/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-page/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/react-page/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-page/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/react-page/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "e76e4f58ef82c6731d07",
-    "url": "/react-page/static/js/main.e76e4f58.chunk.js"
+    "revision": "99b26d68f712917e7c8c",
+    "url": "/react-page/static/js/main.99b26d68.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-page/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/react-page/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-page/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -64,31 +72,23 @@ self.__precacheManifest = [
     "url": "/react-page/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "a1a749e89f578a49306ec2b055c073da",
-    "url": "/react-page/static/media/brand-icons.a1a749e8.svg"
-  },
-  {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/react-page/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/react-page/static/media/outline-icons.82f60bd0.svg"
-  },
-  {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/react-page/static/media/icons.8e3c7f55.eot"
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-page/static/media/brand-icons.a1a749e8.svg"
   },
   {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
     "url": "/react-page/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "e5ace7fcc45b60c66e6e",
-    "url": "/react-page/static/css/1.568d1d1b.chunk.css"
+    "revision": "f6a924b1c43665667956",
+    "url": "/react-page/static/css/1.577d6882.chunk.css"
   },
   {
-    "revision": "c13d9827cfbb38e427fdab8014bfabbb",
+    "revision": "db43bdea1d38ede83adef602ae185a02",
     "url": "/react-page/index.html"
   }
 ];
