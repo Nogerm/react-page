11073353aabcadff60d6f1e8ada14bc48dbc81b7
Andy_CW_Wu <andy_cw_wu@wistron.com>
2018-12-25 11:46:00 +0800

    Update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { LogoImage } from './style';
-import { Grid, Menu, Header } from 'semantic-ui-react'
+import { Grid, Menu, Header, Container } from 'semantic-ui-react'
 import Birthday from './../Birthday';
 import Routine from './../Routine';
 import MondayBless from './../MondayBless/MondayBless';
@@ -16,6 +16,45 @@ class App extends Component {
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  renderMenu = () => {
+    const { activeItem } = this.state;
+
+    return (
+      <Container>
+        <LogoImage src={logo}/>
+        <Header as='h2' style={{ 'margin': '10px', 'color': 'white' }}>Control Center</Header>
+        <Header as='h4' style={{ 'margin': '10px', 'color': 'white' }}>The Backend of Linebot.</Header>
+        <Menu fluid vertical tabular style={{ 'marginTop': '50px' }}>
+          <Menu.Item 
+            name='生日提醒' 
+            active={activeItem === '生日提醒'} 
+            onClick={this.handleItemClick} 
+          />
+          <Menu.Item 
+            name='分享提醒' 
+            active={activeItem === '分享提醒'} 
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='自動回應'
+            active={activeItem === '自動回應'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='週一祝福'
+            active={activeItem === '週一祝福'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='功能設定'
+            active={activeItem === '功能設定'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Container>
+    )
+  }
 
   renderChild = () => {
     const { activeItem } = this.state;
@@ -44,44 +83,14 @@ class App extends Component {
   }
 
   render() {
-    const { activeItem } = this.state;
     const renderChild = this.renderChild;
+    const renderMenu = this.renderMenu;
 
     return (
       <Grid>
         <Grid.Column width={3} style={{ 'background': '#4682B4', 'paddingRight': 0 }}>
-          <LogoImage src={logo}/>
-          <Header as='h2' style={{ 'margin': '10px', 'color': 'white' }}>Control Center</Header>
-          <Header as='h4' style={{ 'margin': '10px', 'color': 'white' }}>The Backend of Linebot.</Header>
-          <Menu fluid vertical tabular style={{ 'marginTop': '50px' }}>
-            <Menu.Item 
-              name='生日提醒' 
-              active={activeItem === '生日提醒'} 
-              onClick={this.handleItemClick} 
-            />
-            <Menu.Item 
-              name='分享提醒' 
-              active={activeItem === '分享提醒'} 
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name='自動回應'
-              active={activeItem === '自動回應'}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name='週一祝福'
-              active={activeItem === '週一祝福'}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name='功能設定'
-              active={activeItem === '功能設定'}
-              onClick={this.handleItemClick}
-            />
-          </Menu>
+          {renderMenu()}
         </Grid.Column>
-
         <Grid.Column stretched width={12}>
           {renderChild()}
         </Grid.Column>
